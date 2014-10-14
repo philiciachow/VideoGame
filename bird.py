1d8abad5123a5127563adf43e1a10e02cf222cac
lianilychee <liani.lye@students.olin.edu>
2014-10-13 22:45:02 -0400

    sprite added

--- bird.py
+++ bird.py
@@ -16,13 +16,17 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption('Flappy Bird!')
 
+# Cloud definition
 cloud = pygame.image.load('images/cloud.png')
 cloud = pygame.transform.scale(cloud, (cloud_width,cloud_height))
 cloud_list = [[100,200],[500,150],[350,50],[800,70]]
 
+# Sprite definition
+sprite = pygame.image.load('images/sprite.png')
 
 while True:     # main game loop
     screen.fill((135,206,250))
+    screen.blit(sprite,(WIDTH/2, HEIGHT/2))
     for i, pos in enumerate(cloud_list):
         screen.blit(cloud,(WIDTH - pos[0], pos[1]))
         cloud_list[i][0] = (pos[0] + 1) % (WIDTH + cloud_width) 
