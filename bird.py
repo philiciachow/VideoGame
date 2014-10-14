1745041ec3730f6685c27f3b128803f8fa2dd54b
lianilychee <liani.lye@students.olin.edu>
2014-10-13 23:21:52 -0400

    bricks randomizing and moving, though at same speed as clouds

--- bird.py
+++ bird.py
@@ -4,18 +4,15 @@
 
 import pygame, sys
 from pygame.locals import *
-
+from random import randint
 
 # Constants
 WIDTH = 800
 HEIGHT = 600
-
 cloud_width = 200
 cloud_height = 100
-
 brick_width = 150
 brick_height = 50
-
 nest_width = 100
 nest_height = 50
 
@@ -41,27 +38,31 @@ sprite = pygame.image.load('images/sprite.png')
 # Creating bricks
 brick = pygame.image.load('images/brick.png')
 brick = pygame.transform.scale(brick, (brick_width,brick_height))
+brick_x = randint(0, WIDTH)
+brick_y = randint(0, HEIGHT)
 
 
 #Creating a free floating nest (maybe later creat an image of the nest on a brick?)
 nest = pygame.image.load('images/nest.png')
 nest = pygame.transform.scale(nest, (nest_width,nest_height))
 
+
 # main game loop
 while True:
-    screen.fill((135,206,250))
-    for i, pos in enumerate(cloud_list):
-        screen.blit(cloud, (WIDTH - pos[0], pos[1]))
-        cloud_list[i][0] = (pos[0] + 1) % (WIDTH + cloud_width) 
+	screen.fill((135,206,250))
+	for i, pos in enumerate(cloud_list):
+		screen.blit(cloud, (WIDTH - pos[0], pos[1]))
+		cloud_list[i][0] = (pos[0] + 0.5) % (WIDTH + cloud_width) 
+		screen.blit(brick, (brick_x - pos[0] + 5, brick_y - pos[1]))
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
 
-    screen.blit(sprite,(WIDTH/4, HEIGHT/4))
-    screen.blit(brick, (WIDTH/2, HEIGHT/2))
-    screen.blit(nest, (WIDTH/2, 5*HEIGHT/6))      
-    
-    pygame.display.update()
-    clock.tick(50)
+	screen.blit(sprite,(WIDTH/4, HEIGHT/4))
+	# screen.blit(brick, (randint(0,WIDTH), randint(0,HEIGHT))
+	screen.blit(nest, (WIDTH/2, 5*HEIGHT/6))      
+	
+	pygame.display.update()
+	clock.tick(50)
