d98b541f49f95d76cf962b70bc34c22423f191ef
lianilychee <liani.lye@students.olin.edu>
2014-10-19 22:24:51 -0400

    key input added

---
+++ controller.py
@@ -0,0 +1,14 @@
+### Video Game Model
+
+### What the objects do and how it moves.
+
+# import pygame, sys
+# from pygame.locals import *
+from random import randint
+
+print "hello world"
+
+response = raw_input("press a key: ")
+
+if repsonse == K_RETURN:
+    print "blank"
