56a43036f27e9663eb60f2dc7ce9979bad97938d
philiciachow <philicia.chow@gmail.com>
2014-10-21 00:44:49 -0400

    modified collisions and user input

--- controller.py
+++ controller.py
@@ -1,12 +1,30 @@
 ### Video game CONTROLLER
 
-''' Controller handles collisions as well as user input actions.
-'''
+"""
+Controller handles collisions as well as user input actions.
+"""
+
 
 def collisions(element1, element2):
-	pass
+    """
+    check to see if the a line is overlapping/colliding with the b line
+    element1.x ------------ element1.x + element1.width
+            element2.x ---------------- element2.x + element2.width
+    """
+
+    return ((((element2.x) - (element1.x + element1.width)) * ((element2.x) - (element1.x))) <= 0 \
+        or (((element1.x) - (element2.x + element2.width)) * ((element1.x + element1.width) - (element2.x + element2.width))) <= 0) \
+        and ((((element2.y) - (element1.y + element1.height)) * ((element2.y) - (element1.y))) <= 0 \
+        or (((element1.y) - (element2.y + element2.height)) * ((element1.y + element1.height) - (element2.y + element2.height))) <= 0)
 
 
-def user_action():
-
-	sprite.y = sprite.y + 2
+def move(element, distance, direction):
+    
+    if direction == 'left':
+        element.x -= distance
+    if direction == 'right':
+        element.x += distance
+    if direction == 'up':
+        element.y -= distance
+    if direction == 'down':
+        element.y += distance
