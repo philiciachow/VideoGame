7f8fa9bf21cf2449e5eb0c75a79ecb2f423996c2
lianilychee <liani.lye@students.olin.edu>
2014-10-20 23:02:23 -0400

    who knows

--- controller.py
+++ controller.py
@@ -2,13 +2,28 @@
 
 ### What the objects do and how it moves.
 
-# import pygame, sys
+import pygame, sys
 # from pygame.locals import *
 from random import randint
 
-print "hello world"
 
-response = raw_input("press a key: ")
+class Move():
 
-if repsonse == K_RETURN:
-    print "blank"
+    def move_sprite():
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_SPACE:
+                    print "SPACE bar pressed"
+
+
+    def move_game_elements():
+        
+
+
+
+# pygame.event.wait = K_SPACE
+
