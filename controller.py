e1a4a36162a1f69ea36ca33798c2f1c087ff1d67
lianilychee <liani.lye@students.olin.edu>
2014-10-21 00:19:02 -0400

    added rest of child classes

---
+++ controller.py
@@ -0,0 +1,12 @@
+### Video game CONTROLLER
+
+''' Controller handles collisions as well as user input actions.
+'''
+
+def collisions(element1, element2):
+	pass
+
+
+def user_action():
+
+	sprite.y = sprite.y + 2
